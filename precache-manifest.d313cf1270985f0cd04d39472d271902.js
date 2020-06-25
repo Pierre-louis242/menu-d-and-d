34291dea0f662a3d40a3886d907380c46aa26ed1
Pierre-Louis Picard <pierrelouis@lucidweb.io>
2020-06-25 14:27:45 +0200

    Updates

---
+++ precache-manifest.d313cf1270985f0cd04d39472d271902.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb52ace3b2971247e89c396c32ec0059",
+    "url": "/menu-d-and-d/index.html"
+  },
+  {
+    "revision": "4667062e6ebaf6f8d672",
+    "url": "/menu-d-and-d/static/css/main.5f9ba8ae.chunk.css"
+  },
+  {
+    "revision": "636bb72d7c9bde084da6",
+    "url": "/menu-d-and-d/static/js/2.d3470634.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/menu-d-and-d/static/js/2.d3470634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4667062e6ebaf6f8d672",
+    "url": "/menu-d-and-d/static/js/main.c7b90d38.chunk.js"
+  },
+  {
+    "revision": "ccd38cac2ad68f128752",
+    "url": "/menu-d-and-d/static/js/runtime-main.6e309332.js"
+  },
+  {
+    "revision": "d5c757c247559d5367aac95aa31f0ad6",
+    "url": "/menu-d-and-d/static/media/AGGIN.d5c757c2.png"
+  },
+  {
+    "revision": "5d2d902cab0100e298c89a97d0619a56",
+    "url": "/menu-d-and-d/static/media/COCKTAILS1.5d2d902c.png"
+  },
+  {
+    "revision": "49d9c729d0bf94bb8501078b59abb21e",
+    "url": "/menu-d-and-d/static/media/COCKTAILS2.49d9c729.png"
+  },
+  {
+    "revision": "da1c859249249f975a99461c6312d80d",
+    "url": "/menu-d-and-d/static/media/COCKTAILS3.da1c8592.png"
+  },
+  {
+    "revision": "4faf0e6ed8bdf7b079cb5204f9842c1b",
+    "url": "/menu-d-and-d/static/media/FOOD1.4faf0e6e.png"
+  },
+  {
+    "revision": "c6723a9f017a4bb70854191c4464c315",
+    "url": "/menu-d-and-d/static/media/FOOD2.c6723a9f.png"
+  },
+  {
+    "revision": "253f5c0236263ddae4eb36d2bcdf426a",
+    "url": "/menu-d-and-d/static/media/FOOD3.253f5c02.png"
+  },
+  {
+    "revision": "5e41c4abe21d420919a9a0a7e2891b16",
+    "url": "/menu-d-and-d/static/media/FOOD4.5e41c4ab.png"
+  },
+  {
+    "revision": "5e4e85e00552742dd12350a07355555f",
+    "url": "/menu-d-and-d/static/media/FOOD5.5e4e85e0.png"
+  },
+  {
+    "revision": "8eb109cc40aed47ad0ce48f9daabf350",
+    "url": "/menu-d-and-d/static/media/FOOD6.8eb109cc.png"
+  },
+  {
+    "revision": "d6389db7031249d2ea59d95b50f3d58d",
+    "url": "/menu-d-and-d/static/media/IMG2.d6389db7.png"
+  },
+  {
+    "revision": "b2a424b9080021526548f968ee7d9890",
+    "url": "/menu-d-and-d/static/media/IMG3.b2a424b9.png"
+  },
+  {
+    "revision": "dcc9804d90d9f4e4425f59e0bc9153fa",
+    "url": "/menu-d-and-d/static/media/IMG4.dcc9804d.png"
+  },
+  {
+    "revision": "27af80a6179ab35225f408e50a1bcb46",
+    "url": "/menu-d-and-d/static/media/IMG5.27af80a6.png"
+  },
+  {
+    "revision": "dd57dc96f7897adf9bad44f2b3d85202",
+    "url": "/menu-d-and-d/static/media/IMG6.dd57dc96.png"
+  },
+  {
+    "revision": "ede0cebec68171a70275cba8825e8b8b",
+    "url": "/menu-d-and-d/static/media/SOFTETALCOOL1.ede0cebe.png"
+  },
+  {
+    "revision": "727c7e5a6109c2d07829716d4cf72447",
+    "url": "/menu-d-and-d/static/media/SOFTETALCOOL2.727c7e5a.png"
+  },
+  {
+    "revision": "51e3e907aaa032012f4b5baaa2acef30",
+    "url": "/menu-d-and-d/static/media/SOFTETALCOOL3.51e3e907.png"
+  },
+  {
+    "revision": "9ac48f0ab9ca58a952ddfa5767e57624",
+    "url": "/menu-d-and-d/static/media/SOFTETALCOOL4.9ac48f0a.png"
+  },
+  {
+    "revision": "f60864d7c46fac8908607e2014593023",
+    "url": "/menu-d-and-d/static/media/SOFTETALCOOL5.f60864d7.png"
+  },
+  {
+    "revision": "48e903f8abfc70abe8cec4914f573d8c",
+    "url": "/menu-d-and-d/static/media/WINE1.48e903f8.png"
+  },
+  {
+    "revision": "466e439d4fc1f95305a7e830715d73da",
+    "url": "/menu-d-and-d/static/media/WINE2.466e439d.png"
+  },
+  {
+    "revision": "f814e344eba07932c1efb46e6491bcea",
+    "url": "/menu-d-and-d/static/media/WINE3.f814e344.png"
+  },
+  {
+    "revision": "d1b084651383a1a865809bd3a78f8a8c",
+    "url": "/menu-d-and-d/static/media/img1.d1b08465.png"
+  },
+  {
+    "revision": "192e7453a91a99e45da96d14f6933be3",
+    "url": "/menu-d-and-d/static/media/logo.192e7453.png"
+  }
+]);
